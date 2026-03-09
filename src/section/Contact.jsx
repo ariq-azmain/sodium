d5e14e5119f1681e3ad6bba5f021168487bd4c3b
ariqazmain <ariqazmain12@gmail.com>
2026-03-09 17:18:36 +0600

    desaible emailjs

--- src/section/Contact.jsx
+++ src/section/Contact.jsx
@@ -29,40 +29,40 @@ const Contact = () => {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
+    // e.preventDefault();
+    // setLoading(true);
 
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "SODIUM",
-          from_email: form.email,
-          to_email: "contact@sodium.com",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. We will get back to you as soon as possible.");
+    // emailjs
+    //   .send(
+    //     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+    //     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+    //     {
+    //       from_name: form.name,
+    //       to_name: "SODIUM",
+    //       from_email: form.email,
+    //       to_email: "contact@sodium.com",
+    //       message: form.message,
+    //     },
+    //     import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    //   )
+    //   .then(
+    //     () => {
+    //       setLoading(false);
+    //       alert("Thank you. We will get back to you as soon as possible.");
 
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
+    //       setForm({
+    //         name: "",
+    //         email: "",
+    //         message: "",
+    //       });
+    //     },
+    //     (error) => {
+    //       setLoading(false);
+    //       console.error(error);
 
-          alert(`Ahh, something went wrong. Please try again. \n Error: ${error}`);
-        }
-      );
+    //       alert(`Ahh, something went wrong. Please try again. \n Error: ${error}`);
+    //     }
+    //   );
   };
   
   return (
